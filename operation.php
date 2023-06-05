4b4fd625257a700739ff201d7633f396717e6941
sorolandry <94006329+sorolandry@users.noreply.github.com>
2023-06-05 22:59:41 +0000

    operation.php

--- operation.php
+++ operation.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+
 // Connexion à la base de données
 $servername = "localhost";
 $username = "root";
@@ -11,22 +11,21 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Traitement de la requête d'enregistrement des participants
+// Traitement de la requête d'enregistrement des participants dans ma b_d
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nom = $_POST["nom"];
     $prenom = $_POST["prenom"];
     $telephone = $_POST["telephone"];
     $email = $_POST["email"];
 
-    // Insérer les données dans la table participants
+    // Insertion des données dans la table participants de la b_d emargement
     $sql = "INSERT INTO participants (nom, prenom, telephone, email) VALUES ('$nom', '$prenom', '$telephone', '$email')";
 
     if ($conn->query($sql) === TRUE) {
+        //juste pour eviter l'ouverture d'une autre page pour la confirmation
         $_SESSION['confirmation_suc'] = " ";
       
-    } else {
-        
-    }
+    } 
 }
 
 // Fermer la connexion à la base de données
